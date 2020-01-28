eb558486f15f330c150afc0fdd48609e8d0bab6b
fabienLbd <fabien.labedade@gmail.com>
2020-01-28 19:40:01 +0100

    add fontawesome

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,3 +14,5 @@ import '../css/app.scss';
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
 const $ = require('jquery');
 require('bootstrap');
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
